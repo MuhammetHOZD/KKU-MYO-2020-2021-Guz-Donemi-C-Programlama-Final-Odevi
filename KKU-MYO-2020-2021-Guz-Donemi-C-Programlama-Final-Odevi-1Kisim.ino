e1fdcde109d61111a7c31df05e709d094144a039
MuhammetHOZD <74905544+MuhammetHOZD@users.noreply.github.com>
2021-01-14 18:55:28 +0300

    Update KKU-MYO-2020-2021-Guz-Donemi-C-Programlama-Final-Odevi-1Kisim.ino

--- KKU-MYO-2020-2021-Guz-Donemi-C-Programlama-Final-Odevi-1Kisim.ino
+++ KKU-MYO-2020-2021-Guz-Donemi-C-Programlama-Final-Odevi-1Kisim.ino
@@ -4,20 +4,20 @@ int main(void){
   printf("\n\n\n\n                       **** HOŞGELDİNİZ ****\n\n"); //Giriş mesajını yapıyoruz  ( \n yazmamızın nedeni ise bir alt satıra yazdır yada geç anlamındadır ).
 
   printf("\n**** ÖĞRENCİ BİLGİLERİ ****\n\n"
-    "AD: MUHAMMET HÜSEYİN\n"
-    "SOYADI: ÖZDEMİR\n"
-    "OKUL NO : 200706014\n"
+    "AD: ADINIZI YAZINIZ\n"
+    "SOYADI: SOYADINIZI YAZINIZ\n"
+    "OKUL NO : OKUL NUMARANIZI YAZINIZ\n"
     "OKULU: KKÜ\n"
     "BÖLÜMÜ: ELEKTRONİK TEKNOLOJİSİ\n"
-    "YAŞADIGI ŞEHİR: ANKARA\n"
-    "YAŞI: 19\n"
-    "DOĞGUM TARİHİ: 30/07/2001\n"); // ögrenci bilgilerimizi ekran yazdırdık 
+    "YAŞADIGI ŞEHİR: YAŞADIGIN ŞEHİRİ YAZINIZ\n"
+    "YAŞI: YAŞINIZI YAZINIZ\n"
+    "DOĞGUM TARİHİ: DOGUM TARİHİNİZİ YAZINIZ\n"); // ögrenci bilgilerimizi ekran yazdırdık 
 
   printf("\n                  DEVAM ETMEK İÇİN ENTER tuşuna basınız: "); //getchar kullana bilmek için bir uyarı mesajı yazdırdık ekrana.
 
  getchar(); // getchar komutunu kullandık (getchar() fonksiyonunu kullanarak klavyeden karakter girmemiz gerekirken, karakter girmek yerine ENTER tuşuna basarsak, getchar() fonksiyonu \n karakterini geri verir).
 
-  printf("\nMerhabalar ben muhammet hüseyin özdemir\n" 
+  printf("\nMerhabalar ben ADINIZI YAZINIZ\n" 
          "bu dersimizin adı C programlama \n"
          "bu kodlamada ise hocamızın dedigi şekilde kodlarımızı yazdık\n"
          "Final ödevi" );// getchar komutunu kullandıktan sonra hocamızın dedigi gibi ekran nitelik yazdırdık
